e9b5820fb84e3f4dd737b0f898e78b2932cf0d6a
Mark wahba <mark.wahba33@gmail.com>
2020-07-05 21:46:16 +0200

    fix bug

--- Packer/provision.sh
+++ Packer/provision.sh
@@ -3,14 +3,13 @@ set -e
 #provision.sh
  sudo apt-get update -y
  echo "apt-get update done."
- #sudo apt-get install  nginx -y
- #sudo service nginx start
+ 
 
-# sudo cp /tmp/index.php /var/www/html/index.php
+ sudo cp /tmp/index.php /var/www/html/index.php
 
 sudo apt-get install apache2 php libapache2-mod-php awscli stress -y
-sudo aws s3 cp s3://nh-onica/index.php /var/www/html
-#cd /var/www/html && sudo git clone http://github.com/alphamusk/aws-metadata-php-page 
+#sudo aws s3 cp s3://Mark-onica/index.php /var/www/html
+
 
 sudo service apache2 restart
 
